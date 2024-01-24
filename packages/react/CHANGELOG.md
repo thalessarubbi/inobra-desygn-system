2325967ed0c2470cade0477b5413b2275acb3398
Thales Sarubbi <thalessarubbi@gmail.com>
2024-01-24 10:26:47 -0300

    fix(components): Add ref to TextInput and adjust styles

---
+++ packages/react/CHANGELOG.md
@@ -0,0 +1,7 @@
+# @inobra-ui/react
+
+## 1.0.1
+
+### Patch Changes
+
+- Add option to pass refrence to TextInput and adjust some styles
--- packages/react/src/components/Checkbox/styles.ts
+++ packages/react/src/components/Checkbox/styles.ts
@@ -20,7 +20,7 @@ export const CheckboxContainer = styled(Checkbox.Root, {
     backgroundColor: '$inobra300',
   },
 
-  '&:focus': {
+  '&:focus, &[data-state="checked"]': {
     border: '2px solid $inobra300',
   },
 })
